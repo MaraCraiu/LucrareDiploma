29219bf0d650b4042b5a7b22c93a98dc79a83a54
GuduleasaAlexandra <bitzu73@yahoo.com>
2021-03-26 08:59:45 +0000

    .

--- rase.php
+++ rase.php
@@ -21,59 +21,10 @@
 </div>
 
 <div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Akita.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\akita inu.jpg" alt="Akita">
+  <a href=http://info.tm.edu.ro:8088/~mguduleasa/LucrareDiploma/Rase/Chow_Chow.php>
+    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Chow Chow.jpg" alt="Afghan Hound">
   </a>
-  <div class="desc">Akita</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Azawakh.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Azawakh.jpg" alt="Azawakh">
-  </a>
-  <div class="desc">Azawakh</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Bichon_Frise.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Bichon Frise.jpg" alt="Bichon Frise">
-  </a>
-  <div class="desc">Bichon Frise</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Boxer.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Boxer.jpg" alt="Boxer">
-  </a>
-  <div class="desc">Boxer</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Bulldog.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Bulldog.jpg" alt="Bulldog">
-  </a>
-  <div class="desc">Bulldog</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Chiuhuahua.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Chihuahua.jpg" alt="Chihuahua">
-  </a>
-  <div class="desc">Chihuahua</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Chow_Chow.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Chow Chow.jpg" alt="Chow Chow">
-  </a>
-  <div class="desc">Chow Chow</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Czechoslovakian.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Czechoslovakian Wolfdog.jpg" alt="Czechoslovakian Wolfdog">
-  </a>
-  <div class="desc">Czechoslovakian Wolfdog</div>
+  <div class="desc">Chow_Chow</div>
 </div>
 
 <div class="gallery">
@@ -83,12 +34,7 @@
   <div class="desc">Dalmatian</div>
 </div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Dandie.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Dandie Dinmont Terrier.jpg" alt="Dandie Dinmont Terrier">
-  </a>
-  <div class="desc">Dandie Dinmont Terrier</div>
-</div>
+
 
 <div class="gallery">
   <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Dobermann.html>
@@ -97,54 +43,7 @@
   <div class="desc">Dobermann</div>
 </div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/English_Cocker.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\English Cocker Spaniel.jpg" alt="English Cocker Spaniel">
-  </a>
-  <div class="desc">English Cocker Spaniel</div>
-</div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Eurasier.html >
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Eurasier.jpg" alt="Eurasier">
-  </a>
-  <div class="desc">Eurasier</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Fila_Brasileiro.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Fila Brasileiro.jpg" alt="Fila Brasileiro">
-  </a>
-  <div class="desc">Fila Brasileiro</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Finnish%20Laoohund.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Finnish Lapphund.jpg" alt="Finnish Lapphund">
-  </a>
-  <div class="desc">Finnish Lapphund</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/French_Bulldog.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\French Bulldog.jpg" alt="French Bulldog">
-  </a>
-  <div class="desc">French Bulldog</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/German_Shepard.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\German Shepherd.jpg" alt="German Shepherd">
-  </a>
-  <div class="desc">German Shepherd</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Glen.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Glen of Imaal Terrier.jpg" alt="Glen of Imaal Terrier">
-  </a>
-  <div class="desc">Glen of Imaal Terrier</div>
-</div>
 
 <div class="gallery">
   <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Golden_Retriever.html>
@@ -153,69 +52,10 @@
   <div class="desc">Golden Retriever</div>
 </div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/GreatDane.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Great Dane.jpg" alt="Great Dane">
-  </a>
-  <div class="desc">Great Dane</div>
-</div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Harrier.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Harrier.jpg" alt="Harrier">
-  </a>
-  <div class="desc">Harrier</div>
-</div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Havanese.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Havanese.jpg" alt="Havanese">
-  </a>
-  <div class="desc">Havanese</div>
-</div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Ibizan_Hound.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Ibizan Hound.jpg" alt="Ibizan Hound">
-  </a>
-  <div class="desc">Ibizan Hound</div>
-</div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Italian_Greyhound.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Italian Greyhound.jpg" alt="Italian Greyhound">
-  </a>
-  <div class="desc">Italian Greyhound</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Jack_Russel.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Jack Russell Terrier.jpg" alt="Jack Russell Terrier"
->
-  </a>
-  <div class="desc">Jack Russell Terrier</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Komondor.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Korean Jindo.jpg" alt="Korean Jindo">
-  </a>
-  <div class="desc">Korean Jindo</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Korean_Jindo.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Kuvasz.jpg" alt="Kuvasz">
-  </a>
-  <div class="desc">Kuvasz</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Kuvasz.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Komondor.jpg" alt="Komondor">
-  </a>
-  <div class="desc">Komondor</div>
-</div>
 
 <div class="gallery">
   <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Labrador.html>
@@ -224,12 +64,6 @@
   <div class="desc">Labrador Retriever</div>
 </div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Lowchen.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Löwchen.jpg" alt="Löwchen">
-  </a>
-  <div class="desc">Löwchen</div>
-</div>
 
 <div class="gallery">
   <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Maltese.html>
@@ -238,47 +72,7 @@
   <div class="desc">Maltese</div>
 </div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/McNab.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\McNab dog.jpg" alt="McNab dog ">
-  </a>
-  <div class="desc">McNab dog </div>
-</div>
 
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/NewGuinea.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\New Guinea singing dog.jpg" alt="New Guinea singing dog">
-  </a>
-  <div class="desc">New Guinea singing dogr</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/NovaScotia.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Nova Scotia Duck Tolling Retriever.jpg" alt="Nova Scotia Duck Tolling Retriever" a>
-  </a>
-  <div class="desc">Nova Scotia Tolling Retriever</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Olde_English.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Olde English Bulldogge.jpg" alt="Olde English Bulldogge">
-  </a>
-  <div class="desc">Olde English Bulldogge</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/OriginalFila.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Original Fila Brasileiro.png" alt="Original Fila Brasileiro">
-  </a>
-  <div class="desc">Original Fila Brasileiro</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Pomerian.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Pomeranian.jpg" alt="Pomeranian">
-  </a>
-  <div class="desc">Pomeranian</div>
-</div>
 
 <div class="gallery">
   <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Pug.html>
@@ -292,68 +86,6 @@
     <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Rottweiler.jpg" alt="Rottweiler">
   </a>
   <div class="desc">Rottweiler</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Rough%20Collie.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Rough Collie.jpg" alt="Rough Collie">
-  </a>
-  <div class="desc">Rough Collie</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Samoyed.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Samoyed.jpg" alt="Samoyed">
-  </a>
-  <div class="desc">Samoyed</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Shih_Tzu.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Shih Tzu.jpg" alt="Shih Tzu">
-  </a>
-  <div class="desc">Shih Tzu</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/St.Bernard.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\St. Bernard (dog).jpg" alt="St. Bernard (dog)">
-  </a>
-  <div class="desc">St. Bernard</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Thai_Ridgeback.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Thai Ridgeback.jpg" alt="Thai Ridgeback">
-  </a>
-  <div class="desc">Thai Ridgeback</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Vizsla.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Vizsla.jpg" alt="Vizsla">
-  </a>
-  <div class="desc">Vizsla</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Volpino_Italiano.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Volpino Italiano.jpg" alt="Volpino Italiano">
-  </a>
-  <div class="desc">Volpino Italiano</div>
-</div>
-
-<div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Weimaraner.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Weimaraner.jpg" alt="Weimaraner">
-  </a>
-  <div class="desc">Weimaraner</div>
-
-</div><div class="gallery">
-  <a href=file:///C:/Users/marru/Documents/Scoala/Info/LucrareDiploma/Rase/Xiasi_Dog.html>
-    <img src="C:\Users\marru\Documents\Scoala\Info\LucrareDiploma\poze caini\Xiasi Dog.jpg" alt="Xiasi Dog.">
-  </a>
-  <div class="desc">Xiasi Dog</div>
 </div>
 
 <div class="gallery">
